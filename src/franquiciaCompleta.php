9788c33633e49ebd9adf33999f1377dd861746e6
juansopo <sopojuanesteban@gmail.com>
2023-09-21 11:22:17 -0300

    considero que ya no es necesario  tener costo fijo en  franquicia completa ya que ahora si tenes un limites de viajes gratis. Entonces cree una variable viajeGratis, como limitador de que no viajen mas de 2 veces, cuando viaja mas de dos veces hacer viaje se ejecuta como normalmente lo hacia su padre tarjeta

--- src/franquiciaCompleta.php
+++ src/franquiciaCompleta.php
@@ -3,13 +3,28 @@ namespace TrabajoSube;
 
 define("COSTO_FIJO", 0);
 Class franquiciaCompleta extends Tarjeta {
-
-    public function consultarSaldo(){
+    protected viajesGratis = 2; 
+    //  considero que no es necesario ya que con la iteracion 3 ya no puede viajar infinito
+    
+    /* public function consultarSaldo(){
         return $COSTO_FIJO;
     }
+    */
+    
     public function hacerViaje($costo){
+        if ( this->viajesGratis < 0 ){
         $this->viajes += 1;
         return TRUE; 
+        }
+        else {
+            if( $this->saldo <  -$costo){
+                echo "no se puede pagar el viaje \n";
+                return FALSE;
+               } 
+           $this->saldo -= $costo;
+           $this->viajes += 1;
+           return TRUE; 
+        }
     }
 }
 
